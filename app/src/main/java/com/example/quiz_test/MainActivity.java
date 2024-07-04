3e4d7cb62dae6924ed5a2df767c19926b8d221c8
Katedy02 <137890205+Katedy02@users.noreply.github.com>
2024-07-04 15:52:09 +0300

    finished project

--- app/src/main/java/com/example/quiz_test/MainActivity.java
+++ app/src/main/java/com/example/quiz_test/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.quiz_test;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+import com.example.quiz_test.HighScoresActivity;
+import com.example.quiz_test.QuizActivity;
+import com.example.quiz_test.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startQuizButton;
+    private Button viewHighScoresButton;
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        startQuizButton = findViewById(R.id.startQuizButton);
+        viewHighScoresButton = findViewById(R.id.viewHighScoresButton);
+        settingsButton = findViewById(R.id.settingsButton);
+
+        startQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewHighScoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HighScoresActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
